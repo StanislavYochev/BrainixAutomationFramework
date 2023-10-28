739a57bf1e9ea8cdc228f7b987b127aa077cfaf5
StanislavYochev <137089794+StanislavYochev@users.noreply.github.com>
2023-10-28 15:48:34 +0300

    Add files via upload
    POM

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage {
+
+    //Elements
+    @FindBy(id = "user-name")
+    WebElement userNameInput;
+    @FindBy(id = "password")
+    WebElement passwordInput;
+    @FindBy(id = "login-button")
+    WebElement loginButton;
+
+    //Constructor + Page Factory initialization
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    //method i.e. user action on the current page
+
+    public boolean isAT() {
+        return loginButton.isDisplayed();
+    }
+
+    public ProductPage login(String userName, String password){
+        userNameInput.click();
+        userNameInput.clear();
+        userNameInput.sendKeys(userName);
+
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+
+        loginButton.click();
+        return new ProductPage(driver);
+    }
+
+}
